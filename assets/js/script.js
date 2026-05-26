56834cafe26ee6e862ba3ef0c22b3a596b809507
Your Name <sahiledu2007@gmail.com>
2026-05-26 22:06:18 +0600

    Add search functionality and theme toggle to enhance user experience

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,79 @@
-console.log("PU CSE Hub Loaded Successfully");
+/* SEARCH */
+
+const searchInput = document.getElementById("searchInput");
+
+if (searchInput) {
+
+  searchInput.addEventListener("keyup", () => {
+
+    const value = searchInput.value.toLowerCase();
+
+    const cards = document.querySelectorAll(".course-card");
+
+    cards.forEach(card => {
+
+      const text = card.innerText.toLowerCase();
+
+      if (text.includes(value)) {
+
+        card.style.display = "block";
+
+      }
+
+      else {
+
+        card.style.display = "none";
+
+      }
+
+    });
+
+  });
+
+}
+
+
+/* THEME TOGGLE */
+
+const themeToggle = document.getElementById("themeToggle");
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "light") {
+
+  document.body.classList.add("light-mode");
+
+  themeToggle.textContent = "☀️";
+
+}
+
+else {
+
+  themeToggle.textContent = "🌙";
+
+}
+
+themeToggle.addEventListener("click", () => {
+
+  document.body.classList.toggle("light-mode");
+
+  const isLight =
+    document.body.classList.contains("light-mode");
+
+  if (isLight) {
+
+    localStorage.setItem("theme", "light");
+
+    themeToggle.textContent = "☀️";
+
+  }
+
+  else {
+
+    localStorage.setItem("theme", "dark");
+
+    themeToggle.textContent = "🌙";
+
+  }
+
+});
